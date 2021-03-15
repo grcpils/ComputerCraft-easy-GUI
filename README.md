caaae75a4946c8ed806d6052874e2acfab0434e0
Pierrick GOUEREC <pierrick.gouerec@epitech.eu>
2021-03-15 11:13:37 +0100

    [+] Adding Groups

--- README.md
+++ README.md
@@ -12,7 +12,8 @@ edit my_project
 
 ## Features
 - [ ] Monitor title
-- [ ] label
+- [ ] Label
+- [x] Groups
 - [x] Buttons
 - [ ] Vertical charts
 - [ ] Horizontal charts
@@ -50,3 +51,43 @@ gui.newButton{label="Button_A", callback=sayHello, Position={2,2}, Size={2,8}, C
 ```
 
 When Button_A is clicked it print `Hello World !` in your computer terminal
+
+### Groups
+
+Create new group:
+```lua
+gui.newGroup{label="group_name", Position={x,y}, Size={height,width}, Color=outline_color}
+
+-- exemple
+
+gui.newGroup{label="Group_A", Position={1,1}, Size={10,14}, Color=colors.gray}
+
+```
+
+## Full interface exemple
+
+```lua
+-- import gui_api
+GUI = require('gui_api')
+
+-- create gui tools
+gui = GUI.newGUI("right")
+
+-- callback functions declaration
+function sayHello () print("Hello") end
+function sayWorld () print("World") end
+
+-- adding buttons
+gui.newButton{label="Hello", callback=sayHello, Position={4,4}, Size={2,10}, Colors={colors.green,colors.lime}}
+gui.newButton{label="World", callback=sayWorld, Position={4,16}, Size={2,10}, Colors={colors.red,colors.orange}}
+
+-- adding group
+gui.newGroup{label="Buttons Group", Position={2,2}, Size={16,26}, Color=colors.gray}
+
+-- start the GUI
+gui.init()
+```
+
+## Screenshot
+
+![gui_api]()
--- gui_api.lua
+++ gui_api.lua
@@ -8,7 +8,7 @@
 table = require 'table'
 
 function newGUI (monitorSide)
-    local self = { monitor = peripheral.wrap(monitorSide), buttons = {}, charts = {}, }
+    local self = { monitor = peripheral.wrap(monitorSide), buttons = {}, charts = {}, groups = {} }
 
     local Position = { x = 0, y = 0 }
     local Size = { h = 0, w = 0 }
@@ -18,29 +18,71 @@ function newGUI (monitorSide)
     -- PRIVATE
     --
 
+    -- printf function like C
     local printf = function(s,...)
         return io.write(s:format(...))
     end
 
+    -- draw a simple Shape with Xmin, Xmax, Ymin, Ymax values
     local drawShape = function (shape)
         for cY = shape.Ymin, shape.Ymax do
-            self.monitor.setCursorPos(shape.Xmin, cY)
-            self.monitor.write(" ")
             for cX = shape.Xmin, shape.Xmax do
+                self.monitor.setCursorPos(cX, cY)
                 self.monitor.write(" ")
             end
         end
     end
 
+    -- draw outline of Shape with Xmin, Xmax, Ymin, Ymax values
+    local drawShapeOut = function (shape)
+        for cY = shape.Ymin, shape.Ymax do
+            for cX = shape.Xmin, shape.Xmax do
+                self.monitor.setCursorPos(cX, cY)
+                if cY == Ymin or cY == Ymax then
+                    self.monitor.write(" ")
+                elseif cX == Xmin or cX == Xmax then
+                    self.monitor.write(" ")
+                end
+            end
+        end
+    end
+
+    -- draw group label
+    local drawGroupLabel = function (grp)
+        local Y = Ymin
+        local X = math.floor((grp.Xmax + grp.Xmin - string.len(grp.label)) / 2)
+        self.monitor.setBackgroundColor(colors.black)
+        self.monitor.setCursorPos(X, Y)
+        self.monitor.write(" " .. grp.label .. " ")
+    end
+
+    -- draw a group
+    local drawGroup = function (grp)
+        self.monitor.setBackgroundColor(grp.Color)
+        drawShapeOut(grp)
+        self.monitor.setBackgroundColor(colors.black)
+    end
+
+    -- foreach in groups table for draw all group
+    local drawAllGroups = function ()
+        table.foreach(self.groups, function (key, grp)
+            printf("[GUI] Draw group '%s' with ID: %s\n", grp.label, key)
+            drawGroup(grp)
+            drawGroupLabel(grp)
+        end)
+    end
+
+    -- draw a button label in center of 'btn' instance
     local drawBtnLabel = function (btn, state)
         local Y = math.floor((btn.Ymin + btn.Ymax) / 2)
-        local X = math.floor((btn.Xmax + btn.Xmin) / 2) - 1
+        local X = math.floor((btn.Xmax + btn.Xmin - string.len(btn.label)) / 2) + 1
         self.monitor.setBackgroundColor(btn.Colors[state])
         self.monitor.setCursorPos(X, Y)
         self.monitor.write(btn.label)
         self.monitor.setBackgroundColor(colors.black)
     end
 
+    -- draw a button with this current state [idle, active]
     local drawButton = function (btn, state)
         self.monitor.setBackgroundColor(btn.Colors[state])
         drawShape(btn)
@@ -48,19 +90,22 @@ function newGUI (monitorSide)
         self.monitor.setBackgroundColor(colors.black)
     end
 
+    -- foreach in buttons table for draw all button
     local drawAllButtons = function ()
         table.foreach(self.buttons, function (key, btn)
-            printf("[GUI] Draw '%s' with ID: %s\n", btn.label, key)
+            printf("[GUI] Draw button '%s' with ID: %s\n", btn.label, key)
             drawButton(btn, "idle")
         end)
     end
 
+    -- flash the button when click on it
     local flashButton = function (btn)
         drawButton(btn, "active")
         sleep(0.2)
         drawButton(btn, "idle")
     end
 
+    -- wait for click action on a button
     local buttonWaitForClick = function (x, y)
         table.foreach(self.buttons, function (key, btn)
             if y >= btn.Ymin and y <= btn.Ymax then
@@ -73,6 +118,7 @@ function newGUI (monitorSide)
         end)
     end
 
+    -- start a loop for catch events
     local waitForEvents = function ()
        while true do
             event, side, x, y = os.pullEvent("monitor_touch")
@@ -80,43 +126,71 @@ function newGUI (monitorSide)
         end
     end
 
+    -- get Xmin, Xmax, Ymin, Ymax with Position and Size
+    local getCoordonate = function (Position, Size)
+        Ymin = math.floor(Position[1])
+        Xmin = math.floor(Position[2])
+        Ymax = math.floor((Position[1] + Size[1]))
+        Xmax = math.floor((Position[2] + Size[2]))
+        return Xmin, Xmax, Ymin, Ymax
+    end
+
     --
     -- PUBLIC
     --
 
+    -- create new group
+    local newGroup = function (Opt)
+        Xmin, Xmax, Ymin, Ymax = getCoordonate(Opt.Position, Opt.Size)
+        local group = {
+            label = Opt.label,
+            Xmin = Xmin,
+            Xmax = Xmax,
+            Ymin = Ymin,
+            Ymax = Ymax,
+            Color = Opt.Color
+        }
+        table.insert(self.groups, group)
+        return
+    end
+
+    -- create new button
     local newButton = function (Opt)
-        Ymin = math.floor(Opt.Position[1])
-        Xmin = math.floor(Opt.Position[2])
-        Ymax = math.floor((Opt.Position[1] + Opt.Size[1]))
-        Xmax = math.floor((Opt.Position[2] + Opt.Size[2]))
-        Colors["idle"] = Opt.Colors[1]
-        Colors["active"] = Opt.Colors[2]
-        button = {
+        local colors = {}
+        Xmin, Xmax, Ymin, Ymax = getCoordonate(Opt.Position, Opt.Size)
+        colors["idle"] = Opt.Colors[1]
+        colors["active"] = Opt.Colors[2]
+        local button = {
             label = Opt.label,
             callback = Opt.callback,
             Ymin = Ymin,
             Xmin = Xmin,
             Ymax = Ymax,
             Xmax = Xmax,
-            Colors = Colors
+            Colors = colors
         }
         table.insert(self.buttons, button)
         return
     end
 
+    -- print all logged buttons in terminal
     local logAllButtons = function ()
         table.foreach(self.buttons, function (key, value)
             printf("[GUI] button '%s' ID: %s\n", key, value.label)
         end)
     end
 
+    -- init GUI (Draw all logged elements and start events loop)
     local init = function ()
+        self.monitor.clear()
+        drawAllGroups()
         drawAllButtons()
         waitForEvents()
     end
 
     return {
         newButton = newButton,
+        newGroup = newGroup,
         logAllButtons = logAllButtons,
         init = init
     }
